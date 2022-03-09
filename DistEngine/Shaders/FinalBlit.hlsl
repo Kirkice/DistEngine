b739bd720776afa210754a9cb495758dbac31370
Kirkice <kirkhan1@outlook.com>
2022-03-09 23:30:02 +0800

    【Kirk】FinalBlit

---
+++ DistEngine/Shaders/FinalBlit.hlsl
@@ -0,0 +1,47 @@
+/* $Header: FinalBlit.hlsl                                           6/26/21 20:55p KirkZhu $ */
+/*--------------------------------------------------------------------------------------------*
+*                                                                                             *
+*                 Project Name : DistEngine                                                   *
+*                                                                                             *
+*                    File Name : FinalBlit.hlsl                                               *
+*                                                                                             *
+*                   Programmer : Kirk Zhu                                                     *
+*                                                                                             *
+*---------------------------------------------------------------------------------------------*/
+#ifndef FINALBLIT_INCLUDE
+#define FINALBLIT_INCLUDE
+
+
+Texture2D gAttachmentColor    								: register(t0);
+
+
+
+
+struct VertexIn
+{ 
+	float3 PosL    											: POSITION;
+	float2 TexC    											: TEXCOORD;
+};
+
+struct VertexOut
+{
+	float4 PosH    											: SV_POSITION;
+	float2 TexC    											: TEXCOORD0;
+};
+
+VertexOut VS(VertexIn vin)
+{
+	VertexOut vout                                      	= (VertexOut)0.0f;
+    vout.PosH 												= float4(vin.PosL, 1.0f);
+	vout.TexC 												= vin.TexC;
+    return vout;
+}
+
+float4 PS(VertexOut pin) : SV_Target
+{
+	// float4 color 										= gRenderTarget.Sample(gsamLinearClamp, pin.TexC); 
+	return float4(1,1,1,1);
+}
+
+#endif
+
