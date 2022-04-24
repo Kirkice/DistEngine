7dc20d244b8561930cb574962cbf62e4323fc832
Kirkice <kirkhan1@outlook.com>
2022-04-24 18:01:32 +0800

    【Kirk】System

---
+++ System/EngineSystem/Texture.h
@@ -0,0 +1,16 @@
+#pragma once
+#include "SystemUtils.h"
+
+namespace Dist
+{
+	struct Texture
+	{
+		// Unique material name for lookup.
+		std::string Name;
+
+		std::wstring Filename;
+
+		Microsoft::WRL::ComPtr<ID3D12Resource> Resource = nullptr;
+		Microsoft::WRL::ComPtr<ID3D12Resource> UploadHeap = nullptr;
+	};
+}
