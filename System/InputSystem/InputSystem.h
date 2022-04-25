61f66d1fbd96fbeb71c23a6aa9a18df9d70b2753
Kirkice <kirkhan1@outlook.com>
2022-04-25 21:58:46 +0800

    【Kirk】InputSystem

--- System/InputSystem/InputSystem.h
+++ System/InputSystem/InputSystem.h
@@ -1,1 +1,32 @@
 #pragma once
+#include "../EngineSystem/EngineApp.h"
+#include "../../Component/Camera.h"
+
+using Microsoft::WRL::ComPtr;
+using namespace DirectX;
+using namespace DirectX::PackedVector;
+
+namespace Dist
+{
+	class InputSystem : public EngineApp
+	{
+
+	public:
+		InputSystem(HINSTANCE hInstance);
+		InputSystem(const InputSystem& rhs) = delete;
+		InputSystem& operator=(const InputSystem& rhs) = delete;
+		~InputSystem();
+
+		void OnMouseDown(WPARAM btnState, int x, int y);
+		void OnMouseUp(WPARAM btnState, int x, int y);
+		void OnMouseMove(WPARAM btnState, int x, int y);
+
+		void OnKeyboardInput(const GameTimer& gt);
+
+	public:
+
+		POINT mLastMousePos;
+		Camera mCamera;
+	};
+
+}
