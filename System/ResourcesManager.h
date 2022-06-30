6aa6a873e088dbe9f157c7716e0ab4a5b7b55625
Kirkice <kirkhan1@outlook.com>
2022-06-30 22:17:16 +0800

    2022/6/30

--- System/ResourcesManager.h
+++ System/ResourcesManager.h
@@ -11,3 +11,5 @@ class ResourcesManager
 public:
 	static bool LoadTextureFromFile(const char* filename, Microsoft::WRL::ComPtr<ID3D12Device> d3d_device, D3D12_CPU_DESCRIPTOR_HANDLE srv_cpu_handle, ID3D12Resource** out_tex_resource, int* out_width, int* out_height);
 };
+
+
