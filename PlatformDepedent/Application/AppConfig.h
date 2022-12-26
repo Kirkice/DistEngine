71d74702b8cb30975cdf44d0c6db3f8519298641
zhu han <kirkhan1@outlook.com>
2022-12-26 21:03:12 +0800

    【Kirk】第三方库

---
+++ PlatformDepedent/Application/AppConfig.h
@@ -0,0 +1,12 @@
+//
+// Created by Administrator on 2022/12/26.
+//
+
+#ifndef DISTENGINE_APPCONFIG_H
+#define DISTENGINE_APPCONFIG_H
+
+#define EnableSkyModel_ (1)
+#define EnableEmbree_ (0)
+#define EnableShaderModel6_ (1)
+
+#endif //DISTENGINE_APPCONFIG_H
